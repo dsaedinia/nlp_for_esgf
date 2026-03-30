2a6054336fb7ea52a5dc0802da6d8904e0fdfa08
dsaedinia <dsaedi96@gmail.com>
2026-03-30 13:27:31 -0400

    models now loaded globally and display top N results

--- find_variables.py
+++ find_variables.py
@@ -8,11 +8,22 @@ from git import Repo
 from rapidfuzz import fuzz
 from sentence_transformers import SentenceTransformer
 
+# Load global models
+nlp = spacy.load("en_core_web_sm")
+sbert_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# ACCEPT_PERCENTILE = 0.70
+TOP_K_RAPID = 15
+TOP_K_FINAL = 10
+
 
 def extract_noun_phrases(text: str) -> list[str]:
-    nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
     return [chunk.text.lower() for chunk in doc.noun_chunks]
+
+    # When adding other facets later with fewer options we will need to extract those phrases out to avoid
+    # a situation like monthly soil moisture content being split into "monthly soil moisture content" and not
+    # monthly, soil moisture content
 
 
 def clone_or_update_tables_repo() -> Repo:
@@ -68,8 +79,6 @@ def make_all_lower(df: pd.DataFrame) -> pd.DataFrame:
 ########################
 # Metrics start here
 #########################
-
-
 def add_specifity(query: str, df: pd.DataFrame):
     """1 if there is an exact match, 0 if not."""
     tokens = query.split()
@@ -82,8 +91,6 @@ def add_specifity(query: str, df: pd.DataFrame):
 def add_SBERT_score(
     query: str, df: pd.DataFrame, columns: list[str] = ["space_cf_standard", "long_name", "comment"]
 ) -> pd.DataFrame:
-    sbert_model = SentenceTransformer("all-MiniLM-L6-v2")
-
     query_embedding = sbert_model.encode(query, convert_to_tensor=True)
 
     def max_SBERT_score(row):
@@ -130,26 +137,24 @@ def find_best_match(df: pd.DataFrame, phrase: str):
     # Specificity --> Exact matches
     df_filtered = add_specifity(phrase, df_filtered)
     if df_filtered["specifity"].any():
-        return df_filtered[df_filtered["specifity"]]
+        return df_filtered[df_filtered["specifity"]].head(TOP_K_FINAL)
 
     # Rapidfuzz score -->  Fast metric for fuzzy matching
     df_filtered = add_rapid_score(phrase, df_filtered)
-    df_filtered = df_filtered[df_filtered["rapid_score"] >= ACCEPT_PERCENTILE]
+    df_filtered = df_filtered.nlargest(TOP_K_RAPID, "rapid_score")
 
-    # SBERT score --> Slower but more semantically aware metric
+    # SBERT score rerank --> Slower but more semantically aware metric
     df_filtered = add_SBERT_score(phrase, df_filtered)
-    df_filtered = df_filtered[df_filtered["max_SBERT_score"] >= ACCEPT_PERCENTILE].sort_values(
-        "max_SBERT_score", ascending=False
-    )
-    return df_filtered
+    df_filtered = df_filtered.sort_values("max_SBERT_score", ascending=False)
+
+    return df_filtered.head(TOP_K_FINAL)
 
 
 if __name__ == "__main__":
-    ACCEPT_PERCENTILE = 0.70
     if len(sys.argv) > 1:
         QUERY_STRING = sys.argv[1]
     else:
-        QUERY_STRING = "search for soil moisture content and tas"
+        QUERY_STRING = "search for soil moisture content, tas and minimum air temperature"
 
     # Initialization
     repo = clone_or_update_tables_repo()
@@ -165,11 +170,12 @@ if __name__ == "__main__":
     variables_found = set()
     for phrase in noun_phrases:
         df_filtered = find_best_match(df, phrase)
+        print(f"Top matches for '{phrase}':")
         if not df_filtered.empty:
+            print(df_filtered)
             variables_found.add((phrase, df_filtered["variable_id"].iloc[0]))
-
     # printing only top variables found per noun phrase
     # NOTE: we could have multiple variables with tied scores but this is defaulting to one for now
-    print("Variables found across all noun phrases: ")
+    print("Top variables found across all noun phrases: ")
     for phrase, var in variables_found:
         print(f"{phrase} --> {var}")
